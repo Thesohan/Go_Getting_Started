cf85d8e440d302493334a41b6f29896bcc95199f
Thesohan <sohan.cse16@nituk.ac.in>
2022-01-06 13:18:44 +0530

    import a module

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
+import "rsc.io/quote"
 import "fmt"
 
 func main() {
     fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Opt())
+	fmt.Println(quote.Hello())
 }
