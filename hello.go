a422538610c6be0655cd48f1859e318a49226002
Thesohan <sohan.cse16@nituk.ac.in>
2022-01-06 15:59:32 +0530

    go functions

--- hello.go
+++ hello.go
@@ -3,10 +3,42 @@ package main
 import "rsc.io/quote"
 import "fmt"
 
+
+type Employee struct {
+	fname string
+	lname string
+}
+
+func (emp Employee)fullname(){
+	fmt.Println(emp.fname+""+emp.lname)
+}
 func main() {
     fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Glass())
 	fmt.Println(quote.Opt())
 	fmt.Println(quote.Hello())
+
+	// Data types
+	var i int
+	var f float64
+	var b bool
+	var s string
+
+   fmt.Printf("%T %T %T %T\n",i,f,b,s) // Prints type of the variable  
+   fmt.Printf("%v   %v      %v  %q     \n", i, f, b, s) //prints initial value of the variable  
+   e1:=Employee{"john","Ponting"}
+   e1.fullname()
+   closure()
+}
+
+func closure(){
+	number:=10
+	squareNumber :=func()(int){
+		number*=number
+		return number
+	}
+	fmt.Println(squareNumber())
+
+	fmt.Println(squareNumber())
 }
